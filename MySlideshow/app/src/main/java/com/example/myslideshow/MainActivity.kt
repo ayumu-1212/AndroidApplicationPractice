0ee31a201e7efe8d64af6cd33c4ab122f0e31c59
ayumu-1212 <abe.ayumu.infratop@gmail.com>
2019-11-13 02:30:56 +0900

    :bookmark: kuboへのヘイトが溜まっていく

--- MySlideshow/app/src/main/java/com/example/myslideshow/MainActivity.kt
+++ MySlideshow/app/src/main/java/com/example/myslideshow/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.myslideshow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.BounceInterpolator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView.setOnClickListener {
+            it.animate().apply {
+                duration = 1000L
+                interpolator = BounceInterpolator()
+                y(it.y + 100.0f)
+            }
+        }
     }
 }
